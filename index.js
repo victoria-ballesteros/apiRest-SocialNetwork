4b91b7791f711ffcf0733587cb257196cfc15e7f
Victoria <mariavictoriaballesp@gmail.com>
2024-02-28 22:11:14 -0400

    feat(init config)

---
+++ index.js
@@ -0,0 +1,11 @@
+//Conexión a base de datos
+
+//Crear servidor Node
+
+//Configurar cors
+
+//Convertir los datos del body a objetos JSON
+
+//Cargar configuración de rutas
+
+//Poner servidor a escuchar peticiones http
