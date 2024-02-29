d0a593772133591e471d53755739e109eb6bf354
Victoria <mariavictoriaballesp@gmail.com>
2024-02-29 15:42:20 -0400

    feat(index.js): added express server settings and connection to database, plus some others basic settings

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
+//Importar dependencias
+const { connection } = require("./database/connection");
+//  pera crear el servidor de Node
+const express = require("express");
+const cors = require("cors");
+const puerto = 8080;
+
 //Conexión a base de datos
+console.log("Starting node application");
+connection();
 
 //Crear servidor Node
+const app = express();
 
-//Configurar cors
+//Configurar cors (Middleware: algo que se ejecuta antes que las propias rutas de la api)
+app.use(cors());
 
 //Convertir los datos del body a objetos JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Cargar configuración de rutas
 
 //Poner servidor a escuchar peticiones http
+app.listen(puerto, () => {
+  console.log("Server running in port " + puerto);
+});
